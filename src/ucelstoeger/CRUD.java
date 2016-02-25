73701cd6559d1854fd16dd6d80ad55fb1cd60c5a
JohannesUcel <u.johannes@hotmail.com>
2016-02-25 09:21:55 +0100

    Fix CRUD + JavaDoc

--- src/ucelstoeger/CRUD.java
+++ src/ucelstoeger/CRUD.java
@@ -4,84 +4,104 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.postgresql.ds.PGSimpleDataSource;
+
 /**
  * Baut Verbindung zur Datenbank auf und fuehrt CRUD Befehle aus
+ * 
  * @author Michael Stoeger, Johannes Ucel
  * @version 23.2.2016
  */
 public class CRUD {
-	private PGSimpleDataSource dbconn;
 	private Connection con;
+	// Definieren der PreparedStatements
 	private final String updateTableSQL = "UPDATE produkt SET gewicht = ? WHERE nummer = ?";
-	private final String deleteTableSQL = "DELETE produkt WHERE nummer =?";
-	private final String insertTableSQL = "INSERT INTO ? VALUES (?)";
-	private final String readTableSQL = "SELECT ? FROM ? WHERE ?";
+	private final String deleteTableSQL = "DELETE FROM produkt WHERE nummer = ?";
+	private final String insertTableSQL = "INSERT INTO produkt VALUES (?,?,?)";
+	private final String readTableSQL = "SELECT * FROM produkt WHERE nummer = ?";
+
 	/**
-	 * Konstruktor
-	 * Baut Verbindung zur Datenbank
+	 * Konstruktor Baut Verbindung zur Datenbank
+	 * 
 	 * @param PGSimpleDataSource
 	 */
 	public CRUD(PGSimpleDataSource dbconn) {
-		this.dbconn = dbconn;
 		try {
 			con = dbconn.getConnection();
 		} catch (SQLException e) {
 			System.err.println("Fehler beim Verbinden zur Datenbank!");
-			e.printStackTrace();
 		}
-	}
-	/**
-	 * Fuegt Daten in Tabelle ein
-	 * Values sind in der Form: value1, value2, value3   einzutragen
-	 * @param table
-	 * @param values
-	 */
-	public void create(String table, String values) {
-		try{
-			PreparedStatement ps = con.prepareStatement(readTableSQL);
-			ps.setString(1, table);
-			ps.setString(2, values);
-			ps.executeQuery();
-		}catch(Exception e){
-			System.err.println("Fehler beim Insert");
-		}
-	}
-	/**
-	 * Gibt Daten aus der Datenbank als Resultset zurueck
-	 * @param what
-	 * @param table
-	 * @param where
-	 * @return Ergebnis der Query als ResultSet
-	 */
-	public ResultSet read(String what, String table, String where) {
-		if(!(allowedString(what)||allowedString(table)||allowedString(where)))
-			return null;
-		try{
-			PreparedStatement ps = con.prepareStatement(readTableSQL);
-			ps.setString(1, what);
-			ps.setString(2, table);
-			ps.setString(3, where);
-			return ps.executeQuery();
-		}catch(Exception e){
-			System.err.println("Fehler beim Lesen");
-		}
-		return null;
 	}
 
 	/**
-	 * Veraendert Daten in der Datenbank
-	 * @param value
-	 * @param where
+	 * Fuegt Daten in Tabelle als Values ein Folgende Syntax (nummer,
+	 * bezeichnung, gewicht)
+	 * 
+	 * @param nummer
+	 *            - Ist ein Integerwert (und PK)
+	 * @param bezeichnung
+	 *            - Eine Produktbeschreibung als String
+	 * @param gewicht
+	 *            - Ist das Gewicht als Integerwert
 	 */
-	public void update(int value, int where) {
+	public void create(int nummer, String bezeichnung, int gewicht) {
+		try {
+			PreparedStatement ps = con.prepareStatement(insertTableSQL);
+			// Erstes "?" mit einem Integerwert (Nummer) ersetzen
+			ps.setInt(1, nummer);
+			// Zweites "?" mit der Produktbeschreibung ersetzen
+			ps.setString(2, bezeichnung);
+			// Drittes "?" mit einem Integerwert (Gewicht) ersetzen
+			ps.setInt(3, gewicht);
+			// PreparedStatement durchführen
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("Fehler beim Inserten");
+		}
+	}
+
+	/**
+	 * Liest aus der Datenbank die Zeile mit der gewünschten Nummer aus
+	 * 
+	 * @param nummer
+	 *            - Die gewünschte Nummer (Integerwert)
+	 */
+	public void read(int nummer) {
+		try {
+			PreparedStatement ps = con.prepareStatement(readTableSQL);
+			// Ersetzt die Nummer (Integerwert)
+			ps.setInt(1, nummer);
+			// Für die Ausgabe der gewünschten Zeile wird ein ResultSet erzeugt
+			ResultSet tmp = ps.executeQuery();
+			while (tmp.next()) {
+				System.out.print("Nummer: " + tmp.getInt(1));
+				System.out.print(", Bezeichnung: " + tmp.getString(2));
+				System.out.print(", Gewicht: " + tmp.getInt(3));
+			}
+		} catch (Exception e) {
+			System.err.println("Fehler beim Lesen");
+		}
+	}
+
+	/**
+	 * Veraendert bzw. Updated Daten in der Datenbank
+	 * 
+	 * @param gewicht
+	 *            - Das gewünschte Gewicht als Integerwert
+	 * @param where
+	 *            - Die Produktnummer (Integerwert)
+	 */
+	public void update(int gewicht, int where) {
 		try {
 			PreparedStatement ps = con.prepareStatement(updateTableSQL);
-			ps.setInt(1, value); 
-			ps.setInt(2, where); 
-			ps.executeUpdate(); 
+			ps.setInt(1, gewicht);
+			ps.setInt(2, where);
+			ps.executeUpdate();
+			System.out.println();
 			System.out.println("Update erfolgreich!");
+			read(where);
 		} catch (SQLException e) {
 			System.err.println("Fehler beim Updaten!");
 		}
@@ -89,44 +109,44 @@ public class CRUD {
 
 	/**
 	 * Loescht Daten aus der Datenbank
+	 * 
 	 * @param where
+	 *            - Gibt die gewünschte Produktnummer an
 	 */
 	public void delete(int where) {
 		try {
 			PreparedStatement ps = con.prepareStatement(deleteTableSQL);
-			ps.setInt(1, where);  
-			ps.executeQuery();
+			ps.setInt(1, where);
+			ps.executeUpdate();
 			System.out.println("Loeschen erfolgreich");
 		} catch (SQLException e) {
 			System.err.println("Fehler beim Loeschen!");
+			e.printStackTrace();
 		}
 	}
+
 	/**
-	 * Generiert Zufallsdaten in die "produkt" Tabelle
-	 * @param count <- Wie viele Zeilen
+	 * Generiert Zufallsdaten in der "produkt" Tabelle
+	 * 
+	 * @param count
+	 *            <- Wie viele Zeilen
 	 */
-	public void dataFiller(int count){
-		ResultSet maxnumber = read("max(nummer)","produkt", "true");
-		String tmp ="";
+	public void dataFiller(int count) {
+		int maxnummer = 0;
 		try {
-			maxnumber.next();
-			tmp = maxnumber.getString(1);
-		} catch (SQLException e) {
-			System.err.println("Fehler beim Auswerten");
-		}
-		int begin = Integer.parseInt(tmp)+1;
-		for(int i=0;i<count;++i,++begin){
-			String values = Integer.toString(begin)+","+"TEST"+",1";
-			create("produkt", values);
-		}
-	}
-	public boolean allowedString(String toCheck){
-		final String allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPPQRSTUVWXYZ1234567890_()";
-		for(int i=0;i<toCheck.length();++i){
-			if(allowedChars.indexOf(toCheck.charAt(i))==-1){
-				return false;
+			Statement tmp = con.createStatement();
+			// Holt sich die aktuelle Maxnummer, um zukünftige Fehler mit der
+			// falschen Produktnummer zu verhindern
+			ResultSet tmp2 = tmp.executeQuery("SELECT max(nummer) FROM produkt;");
+			while (tmp2.next()) {
+				maxnummer = (tmp2.getInt(1));
 			}
+		} catch (SQLException e) {
+			System.out.println("Fehler beim Lesen der Maxnummer!");
 		}
-		return true;
+		int begin = maxnummer + 1;
+		for (int i = 0; i < count; i++, begin++)
+			create(begin, "TEST" + begin, begin + 1);
+
 	}
 }
--- src/ucelstoeger/Main.java
+++ src/ucelstoeger/Main.java
@@ -1,12 +1,20 @@
 package ucelstoeger;
 
+/**
+ * Zentrale Main - Klasse, wo die CRUD - Methoden aufgerufen werden
+ * 
+ * @author Johannes Ucel, Michael Stöger
+ * @version 25. Feb. 2016
+ *
+ */
 public class Main {
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		InitConnect init = new InitConnect(args);
 		CRUD cr = init.getCr();
-		cr.dataFiller(5);
+		cr.read(3);
 	}
 }
